21b927f26688fea5fa9780ce53e75f25be12c7cd
reireynoso <reireynoso@gmail.com>
2020-01-06 12:14:15 -0500

    sessions controller updated for login and auto_login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,9 +1,20 @@
 class Api::V1::SessionsController < ApplicationController
     def login
-
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            payload = {user_id: user.id}
+            token = encode_token(payload)
+            render json: {user: user, token: token}
+        else
+            render json: {errors: ["Invalid Credentials"]}, status: :not_acceptable
+        end
     end
 
     def auto_login
-
+        if session_user
+            render json: session_user
+        else
+            render json: {errors: ["No user logged in"]}
+        end
     end
 end
