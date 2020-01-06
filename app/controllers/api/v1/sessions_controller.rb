16c437a196a89f90f824348575eff8d5f92819e6
reireynoso <reireynoso@gmail.com>
2020-01-06 11:18:39 -0500

    template setup for handling login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::SessionsController < ApplicationController
+    def login
+
+    end
+
+    def auto_login
+
+    end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::UsersController < ApplicationController
+
+    def create
+
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :photo_url_string)
+    end
 end
