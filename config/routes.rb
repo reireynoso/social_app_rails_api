91a60e398976273882b35014922fbf66ab79bbba
reireynoso <reireynoso@gmail.com>
2020-01-06 11:16:07 -0500

    generated sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       resources :users, only: [:create]
+      post "/login", to: "sessions#login"
+      get "/auto_login", to: "sessions#auto_login"
     end
   end
 end
