1ad091d20a6ba54d7f091043c612339bed4ab6ff
reireynoso <reireynoso@gmail.com>
2020-01-06 11:37:52 -0500

    user controller create method setup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::UsersController < ApplicationController
 
     def create
-
+        user = User.create(user_params)
+        if user.valid? 
+            payload = {user_id: user.id}
+            token = encode_token(payload)
+            render json: {user: user, token: token}
+        else
+            render json: {errors: user.errors.full_messages}, status: :not_acceptable
+        end
     end
 
     private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates_uniqueness_of :username
+    validates_presence_of :username
+    validates_presence_of :password_digest
 end
