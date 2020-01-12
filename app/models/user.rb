7831439be0daed65d362b5884cf76271fb42429a
reireynoso <reireynoso@gmail.com>
2020-01-12 15:06:09 -0500

    user model modified aliasing

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,9 @@ class User < ApplicationRecord
     has_many :posts, :dependent => :delete_all
     
     has_many :followed_users, foreign_key: :follower_id, class_name: "Follow"
-    has_many :followees, through: :followed_users
+    has_many :followees, through: :followed_users, source: :person_being_followed
 
     has_many :following_users, foreign_key: :followee_id, class_name: "Follow"
-    has_many :followers, through: :following_users
+    has_many :followers, through: :following_users, source: :person_doing_the_following
 
 end
