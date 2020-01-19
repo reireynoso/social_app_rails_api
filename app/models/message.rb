3c0f56075d48b69e16c82e73dd1fd1c27a24b2fc
reireynoso <reireynoso@gmail.com>
2020-01-19 09:36:48 -0500

    modified message model association

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,4 @@
 class Message < ApplicationRecord
+    belongs_to :person_sending_the_message, class_name: "User", foreign_key: "sender_id"
+    belongs_to :person_receving_the_message, class_name: "User", foreign_key: "receiver_id"
 end
