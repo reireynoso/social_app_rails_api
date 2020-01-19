fe5da0aa1299623fcd3983d6ebc94f9aa844da3f
reireynoso <reireynoso@gmail.com>
2020-01-19 09:37:22 -0500

    modified user association to message

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,9 @@ class User < ApplicationRecord
     has_many :following_users, foreign_key: :followee_id, class_name: "Follow"
     has_many :followers, through: :following_users, source: :person_doing_the_following
 
+    has_many :sent_messages, foreign_key: :sender_id, class_name: "Message"
+    has_many :receivers, through: :sent_messages, source: :person_receving_the_message
+    has_many :received_messages, foreign_key: :receiver_id, class_name: "Message"
+    has_many :senders, through: :received_messages, source: :person_sending_the_message
+
 end
